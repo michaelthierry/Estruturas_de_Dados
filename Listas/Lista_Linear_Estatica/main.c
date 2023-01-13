3d3057e981d76f131ad7957f0cb739117f4a2594
Michael Thierry <michaelthierry86@gmail.com>
2023-01-13 19:46:50 -0400

    feat: add menu to user

--- Listas/Lista_Linear_Estatica/main.c
+++ Listas/Lista_Linear_Estatica/main.c
@@ -23,9 +23,44 @@ int main(){
     lista = lista_criar();
     //Verifica se a lista foi criada
     if(lista != NULL){
+        //Mostrando informações da lista
         printf("@> Lista criada\n"
                "@> Tipo: Lista Linear Estatica\n"
                "@> Tamanho: %d\n", MAX);
+        //Variavel de opção
+        int opcao;
+        do{
+            //Mostrando as opções 
+            printf("+------------------------------------------+\n"
+                   "|               OPERACOES                  |\n"
+                   "+------------------------------------------+\n"
+                   "|[1] Inserir no inicio da lista            |\n"
+                   "|[2] Inserir no meio da lista (ordenado)   |\n"
+                   "|[3] Inserir no fim da lista               |\n"
+                   "|[4] Remover do inicio da lista            |\n"
+                   "|[5] Remover do meio (por valor)           |\n"
+                   "|[6] Remover do fim da lista               |\n"
+                   "|[7] Mostrar lista                         |\n"
+                   "|[8] Consulta (por posição)                |\n"
+                   "|[9] Consulta (por valor)                  |\n"
+                   "|[0] Sair                                  |\n"
+                   "+------------[Inserira-sua-opção]----------+\n");
+            //lendo do usuario
+            scanf("%i", &opcao);
+            //Avaliando a opção
+            switch(opcao){
+            case 0:
+                break;
+            case 7:
+                lista_mostrar(lista);
+                printf("@> Lista mostrada\n");
+                break;       
+            default:
+                printf("@> Opção Invalida!\n");
+                break;
+            }
+
+        }while(opcao != 0);
         
     }else{
         printf("@> Erro ao criar a lista\n");
